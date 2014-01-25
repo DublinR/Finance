101d757b669a7748ba94cec8b9bf319e41b18394
Dublin R <DublinRUserGroup@gmail.com>
2014-01-24 20:00:05 -0800

    Create QM-chartTheme.R

---
+++ QM-chartTheme.R
@@ -0,0 +1,9 @@
+chartTheme()
+chartTheme('white')
+chartTheme('white',up.col='blue',dn.col='red')
+ 
+# A TA example
+chartTheme(addRSI.col='red')
+ 
+str(chartTheme())
+#########################################################################
